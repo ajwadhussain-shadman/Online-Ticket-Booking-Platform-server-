7cdb0a44380ed1bcc899f66407175e747847adba
ajwadhussain-shadman <ajwadhussain38540@gmail.com>
2026-06-23 09:07:43 +0600

    added ticket delete route

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port =process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
@@ -54,7 +54,15 @@ async function run() {
       const result= await ticketsCollection.insertOne(newTicket);
       res.send(result);
     })
-
+      
+    app.delete('/api/tickets/:id', async(req,res)=>{
+      const id= req.params.id;
+      const query={
+        _id: new ObjectId(id)
+      }
+      const result= await ticketsCollection.deleteOne(query);
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
